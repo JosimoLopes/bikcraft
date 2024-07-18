3512354b48b79ccf296261eb303ada249d944b52
Josimo Lopes <joe.lopes@outlook.com>
2024-07-18 19:46:13 -0400

    js alert

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const links = document.querySelectorAll("a");
+
+console.log(links);
+
+links.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.alert("Sorry, nothing to see here yet. Please, come back later.");
+  });
+});
